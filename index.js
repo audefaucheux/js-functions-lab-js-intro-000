0c4bc54d3fc03b7dfa8c7f61f592b71b66a5eb2d
Aude Faucheux <aude53@gmail.com>
2019-05-12 18:14:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ function happyHolidays(string) {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holiday, ${name}!`
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday,day) {
+  return `It's ${day}, days until ${holiday}!`
 }
